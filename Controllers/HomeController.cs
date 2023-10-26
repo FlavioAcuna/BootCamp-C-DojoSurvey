5dc3e58aae45d8e74c13455c3f426ef07e23a26c
FlavioAcuña <flavio.acuna.t@gmail.com>
2023-10-26 02:44:01 -0300

    V1.0

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,45 @@
+﻿using System.Diagnostics;
+using Microsoft.AspNetCore.Mvc;
+using DojoSurvey.Models;
+
+namespace DojoSurvey.Controllers;
+
+public class HomeController : Controller
+{
+    private readonly ILogger<HomeController> _logger;
+
+    public HomeController(ILogger<HomeController> logger)
+    {
+        _logger = logger;
+    }
+
+    public IActionResult Index()
+    {
+        return View();
+    }
+
+    public IActionResult Privacy()
+    {
+        return View();
+    }
+
+
+    [HttpPost("ProcesarDatos")]
+    public IActionResult ProcesarDatos(string txtName, string locationDojo, string favoriteLanguage, string txareaComment)
+    {
+        Console.WriteLine($"{txtName} {locationDojo} {favoriteLanguage} {txareaComment}");
+        ViewBag.textname = txtName;
+        ViewBag.locadojo = locationDojo;
+        ViewBag.favolang = favoriteLanguage;
+        ViewBag.txtarea = txareaComment;
+        return View("SubmittedForm");
+    }
+
+
+
+    [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+    public IActionResult Error()
+    {
+        return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+    }
+}
